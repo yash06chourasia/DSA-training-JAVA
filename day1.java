d75356d14a625a635c8fba350ada683bf0b4f049
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-02 12:05:17 +0530

    add

--- day1.java
+++ day1.java
@@ -1,26 +1,35 @@
+
 public class day1 {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,0,5,6,7,8};
-        int max=arr[0];
-        int min=arr[0];
-        System.out.println("Hello World");
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+        int arr[]={1,2,3,4};
+        // int max=arr[0];
+        // int min=arr[0];
+        int sum=0;
+        // System.out.println("Hello World");
+        
       
-         for(int i=1;i<arr.length;i++){
-             if(arr[i]>max){
-                max=arr[i];
-             }
-             if(arr[i]<min){
-                min=arr[i];
-             }
+        //  for(int i=1;i<arr.length;i++){
+        //      if(arr[i]>max){
+        //         max=arr[i];
+        //      }
+        //      if(arr[i]<min){
+        //         min=arr[i];
+        //      }
+        //  }
+        //  System.out.println("Maximum is "+max);
+        //    for(int i=arr.length-1;i>=0;i--){
+        //     System.out.println(arr[i]);
+        // }
+        //  System.out.println("Minimum is "+min);
+         for(int i=0;i<arr.length;i++){
+              sum=sum+arr[i];
          }
-         System.out.println("Maximum is "+max);
-           for(int i=arr.length-1;i>=0;i--){
+        //  System.out.println("sum is "+sum);
+         int count=arr.length;
+         float avg=(float)sum/count;
+         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
-         System.out.println("Minimum is "+min);
-         
+         System.out.println("Average is "+avg);
     }
 }
