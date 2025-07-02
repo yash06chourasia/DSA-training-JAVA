1a397365daca517c4a020ade3fc6f1b86d3b6467
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-02 12:26:43 +0530

    add

--- userInput.java
+++ userInput.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class userInput {
     public static void main(String[] args) {
-        Scanner inp=new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         System.out.println("Enter the size of array ");
         int n=inp.nextInt();
         int arr[]=new int[n];
@@ -13,5 +13,12 @@ public class userInput {
         for(int i=0;i<n;i++){
           System.out.println(arr[i]);
         }
+        int max=arr[0];
+        for(int i=1;i<n;i++){
+          if(arr[i]>max){
+            max=arr[i];
+          }
+        }
+        System.out.println("Maximum is "+max);
     }
 }
