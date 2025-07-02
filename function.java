fac2038793a4f27b35776bccb65b565034c3d0b5
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-02 20:35:14 +0530

    add

---
+++ function.java
@@ -0,0 +1,23 @@
+
+public class function {
+   
+    static void print(){
+    System.out.println("Hello World");
+   }
+   
+   static void max(){
+    int arr[]={1,5,2,4,3};
+    int max=arr[0];
+     for(int i=1;i<arr.length;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+     }
+    System.out.println("Maximum "+max);
+   }
+
+   public static void main(String[] args) {
+        print();
+        max();
+    }
+}
