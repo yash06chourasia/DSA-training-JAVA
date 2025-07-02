95f2e3138b9a5451a21a8a27011d0e2d4c226ae2
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-02 10:52:10 +0530

    add

---
+++ day1.java
@@ -0,0 +1,26 @@
+public class day1 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,0,5,6,7,8};
+        int max=arr[0];
+        int min=arr[0];
+        System.out.println("Hello World");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+      
+         for(int i=1;i<arr.length;i++){
+             if(arr[i]>max){
+                max=arr[i];
+             }
+             if(arr[i]<min){
+                min=arr[i];
+             }
+         }
+         System.out.println("Maximum is "+max);
+           for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+         System.out.println("Minimum is "+min);
+         
+    }
+}
