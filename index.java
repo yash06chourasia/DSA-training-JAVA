3db8f271a5e7b06c261e873cabbd0eca41baba72
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-02 10:39:52 +0530

    add

---
+++ index.java
@@ -0,0 +1,9 @@
+public class index {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8};
+        System.out.println("Hello World");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
