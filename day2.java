96776c7b892d255b3325d3aea5a05dc734f5879d
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-03 12:52:05 +0530

    add

--- day2.java
+++ day2.java
@@ -112,9 +112,7 @@
 //                  if(arr1[i]==arr2[i]){
 //                     equal=true;
 //                  }
-//                  else{
 //                     break;
-//                  }
 //           }
            
 //         }
@@ -125,5 +123,67 @@
 //             System.out.println("Not Equal");
 //         }
 //      }
+// }
+
+// 2D ARRAY
+
+
+// public class day2 {
+//     public static void main(String[] args) {
+//     int arr[][]={{1,2,3},{4,5,6/*,7*/},{8,9,0}};
+
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                     System.out.print(arr[i][j]+" ");
+//                 }
+//                 System.out.println();
+//             }
+//         }
+// }
+
+//Sum of elements in 2D array
+
+// public class day2 {
+//     public static void main(String[] args) {
+//     int arr[][]={{1,2,3},{4,5,6},{8,9,0}};
+//     int sum=0;
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                    sum+=arr[i][j];
+//                 }
+//             }
+//         System.out.println("Sum is "+sum);  
+//         }
+// }
+
+//Row wise sum in 2D array
+
+// public class day2 {
+//     public static void main(String[] args) {
+//     int arr[][]={{1,2,3},{4,5,6},{8,9,0}};
+//         for(int i=0;i<arr.length;i++){
+//             int sum=0;
+//             for(int j=0;j<arr[i].length;j++){
+//                    sum+=arr[i][j];
+//                 }
+//         System.out.println("Sum of "+(i+1)+" row is "+sum);  
+//             }
+//         }
+// }
+
+// Column wise sum in 2D array
+
+// public class day2 {
+//     public static void main(String[] args) {
+//     int arr[][]={{1,2,3},{4,5,6},{8,9,0}};
+//         for(int i=0;i<arr.length;i++){
+//             int sum=0;
+//             for(int j=0;j<arr[i].length;j++){
+//                    sum+=arr[j][i];
+//                 }
+//                  System.out.println("Sum of column "+(i+1)+" is "+sum); 
+//             }
+        
+//         }
 // }
 
