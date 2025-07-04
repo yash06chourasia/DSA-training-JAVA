4c4af25833216c1bfcf2bfdc7ff51aa393eb78ec
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-04 20:36:45 +0530

    add

---
+++ Array_to_num.java
@@ -0,0 +1,19 @@
+public class Array_to_num {
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int num=0;
+        int count=1;
+        int sum=0;
+        for(int i=1;i<arr.length;i++){
+            count=count*10;
+        }
+        for(int i=0;i<arr.length;i++){
+           num=num+arr[i]*count;
+           sum=sum+num;
+           count=count/10;
+        }
+        System.out.println(num);
+
+    }
+}
