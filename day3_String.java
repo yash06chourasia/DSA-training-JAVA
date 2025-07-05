a9f3cf0464533c32980fd8f995ce099d94056587
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-05 19:44:15 +0530

    add

--- day3_String.java
+++ day3_String.java
@@ -1,66 +1,46 @@
-public class day3_String {
-    public static void main(String[] args) {
+// public class day3_String {
+//     public static void main(String[] args) {
 
-        // String declaration and initialization along with printing
-        // String str="Hello World";
-        // System.out.println(str);
+//         // String declaration and initialization along with printing
+//         // String str="Hello World";
+//         // System.out.println(str);
 
-        // // String Traversal
-        // for(int i=0;i<str.length();i++){
-        //     System.out.println(str.charAt(i));
-        // }
+//         // // String Traversal
+//         // for(int i=0;i<str.length();i++){
+//         //     System.out.println(str.charAt(i));
+//         // }
 
-        // // Reverse traversal of String
-        // System.out.println("Reverse Traversal");
-        // for(int i=str.length()-1;i>=0;i--){
-        //   System.out.println(str.charAt(i));
-        // }
+//         // // Reverse traversal of String
+//         // System.out.println("Reverse Traversal");
+//         // for(int i=str.length()-1;i>=0;i--){
+//         //   System.out.println(str.charAt(i));
+//         // }
 
-        // // Reversing the string
-        // String rev="";
-        // for(int i=str.length()-1;i>=0;i--){
-        //    rev=rev+str.charAt(i);
-        // }
-        // System.out.println("Reversed String is "+rev);
+//         // // Reversing the string
+//         // String rev="";
+//         // for(int i=str.length()-1;i>=0;i--){
+//         //    rev=rev+str.charAt(i);
+//         // }
+//         // System.out.println("Reversed String is "+rev);
 
-        // // Remove duplicate elements from String
-        // String str="Hello";
-        // String result="";
-        // for(char i=0;i<str.length();i++){
-        //     char ch=str.charAt(i);
-        //     if(result.indexOf(ch)==-1){
-        //         result+=ch;
-        //     }
-        // }
-        // System.out.println(result);
+//         // // Remove duplicate elements from String
+//         // String str="Hello";
+//         // String result="";
+//         // for(char i=0;i<str.length();i++){
+//         //     char ch=str.charAt(i);
+//         //     if(result.indexOf(ch)==-1){
+//         //         result+=ch;
+//         //     }
+//         // }
+//         // System.out.println(result);
 
-        // toCharArray function
-        // String str="Hello";
-        // char data[]=str.toCharArray();
-        // for(char i=0;i<data.length;i++){
-        //     System.out.println(data[i]);
-        // }
+//         // toCharArray function
+//         // String str="Hello";
+//         // char data[]=str.toCharArray();
+//         // for(char i=0;i<data.length;i++){
+//         //     System.out.println(data[i]);
+//         // }
 
-        // Frequency Count
-         String str="Hello";
-         char data[]=str.toCharArray();
-         String done="";
-       
-         for(char i=0;i<data.length;i++){
-            int count=1;
-            char ch=str.charAt(i);
-            if(done.indexOf(ch)==-1){
-                done+=ch;
-            for(int j=i+1;j<data.length;j++){
-
-                if(str.charAt(i)==str.charAt(j)){
-                     count+=1;
-                }
-            }
-            }
-             System.out.println(str.charAt(i)+" occurs "+count+" times");
-         }
-
-
-    }
-}
+//         // Frequency Count
+//     }
+// }
