c0f33b7f500195ffe84ae427848d45e875f96b22
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-17 13:39:36 +0530

    add

--- day9.java
+++ day9.java
@@ -22,14 +22,17 @@
         head=newNode;
     }
     public void addToLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
         Node temp=head;
         while(temp.next!=null){
             temp=temp.next;
         }
-        Node newNode=new Node(data);
+        
         temp.next=newNode;
-        newNode.next=null;
-
     }
     void display(){
         Node temp=head;
@@ -38,12 +41,54 @@
             temp=temp.next;
         }
     }
+    void addToIndex(int data,int index){
+        Node newNode=new Node(data);
+        if(index==0){
+            newNode.next=head;
+            head=newNode;
+            return;
+        }
+        Node temp=head;
+        for(int i=0;i<index-1 && temp!=null;i++){
+            temp=temp.next;
+        }
+        if (temp == null) {
+        System.out.println("Invalid index");
+        return;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
+    public void deleteFirst(){
+       head=head.next;
+    }
+
+     public void deleteLast(){
+       Node temp=head;
+        if (head == null) {
+        return;
+        }
+        if (head.next == null) {
+        head = null;
+        return;
+        }
+       while(temp.next.next!=null){
+        temp=temp.next;
+       }
+       temp.next=null;
+    }
    public static void main(String[] args) {
         day9 list=new day9();
         list.addToFirst(5);
         list.addToLast(10);
         list.addToLast(15);
+        list.addToFirst(1);
+        list.addToIndex(8,0);
+        list.deleteFirst();
+        list.deleteLast();
         list.display();
+
     
    } 
 }
