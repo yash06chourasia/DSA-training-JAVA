494a97b32415364933bfdcf5161e2f34b9099055
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-18 15:00:11 +0530

    add

--- practice2.java
+++ practice2.java
@@ -73,24 +73,159 @@
 
 // WAP  to return the new array after removing target
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-public class practice2{
-    public static void main(String[] args) {
-        int[] arr={1,2,3,4,6,5,6,7};
-        int target=6;
-        int[] newArr=new int[arr.length];
-        int idx=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=target){
-                newArr[idx]=arr[i];
-                idx++;
-            }
-    }
-    int trim[]=Arrays.copyOf(newArr, idx);
-    for(int i=0;i<trim.length;i++){
-        System.out.println(trim[i]);
-    }
-    
-    }
-}
+// public class practice2{
+//     public static void main(String[] args) {
+//         int[] arr={1,2,3,4,6,5,6,7};
+//         int target=6;
+//         int[] newArr=new int[arr.length];
+//         int idx=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]!=target){
+//                 newArr[idx]=arr[i];
+//                 idx++;
+//             }
+//     }
+//     int trim[]=Arrays.copyOf(newArr, idx);
+//     for(int i=0;i<trim.length;i++){
+//         System.out.println(trim[i]);
+//     }
+
+//     }
+// }
+
+// Reverse an array
+// public class practice2 {
+
+//     public static void main(String[] args) {
+//         int arr[]={1,2,5,6,7};
+//         int i=0;
+//         int j=arr.length-1;
+//         while(i<j){
+//             int temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//             i++;
+//             j--;
+//         }
+//         for(int k=0;k<arr.length;k++){
+//             System.out.print(arr[k]+" ");
+//         }
+//     }
+// }
+
+// Check , is the array is palindrome 
+
+//  public class practice2 {
+//     public static void main(String[] args) {
+//         int arr[]={1,3,4,2,5,6,1};
+//         int len=arr.length;
+//         int rev[]=new int[len];
+//         // New array with reverse element
+//         for(int i=0;i<len;i++){
+//             rev[len-i-1]=arr[i];
+//         }
+//         boolean chk=true;
+//         for(int k=0;k<arr.length;k++){
+//             if(rev[k]!=arr[k]){
+//                 chk=false;
+//                 break;
+//             }
+//         }
+//         if(chk){
+//             System.out.println("Palindrome");
+//         }else{
+//             System.out.println("Not a Palindrome");
+//         }
+//     }
+// }
+
+// Merge two sorted arrays
+// public class practice2 {
+
+//     public static void main(String[] args) {
+//         int[]a={1,2,3,4,5};
+//         int[]b={3,6,8,9};
+//         int []m=new int[a.length+b.length];
+//         int i=0,j=0,k=0;
+        // while(i<a.length && j<b.length){
+        //     if(a[i]<b[j]){
+        //         m[k]=a[i];
+        //         i++;
+        //     }
+        //     else{
+        //         m[k]=b[j];
+        //         j++;
+        //     }
+        //     k++;
+        // }
+        // while (i < a.length) {
+//         m[k] = a[i];
+//         k++;
+//         i++;
+//         }
+
+//         while (j < b.length) {
+//         m[k] = b[j];
+//         k++;
+//         j++;
+//         }
+//         for(int o=0;o<m.length;o++){
+//             System.out.print(m[o]+" ");
+//         }
+
+//     }
+// }
+
+// Search in 2D array
+// public class practice2 {
+
+//     public static void main(String[] args) {
+//         int[][]arr={{1,2,3},{4,5,6},{7,8,9}};
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//         int target=10;
+//          for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                 if(arr[i][j]==target){
+//                    System.out.println("Element found at row "+(i+1)+" column "+(j+1)); 
+//                    return;
+//                 }
+//             }
+//         }
+//         System.out.println("Element not found");
+
+//     }
+// }
+
+// Sum  and average of 2D array 
+// public class practice2 {
+
+//     public static void main(String[] args) {
+//         int[][]arr={{1,2,3},{4,5,6},{7,9,9}};
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+        //     System.out.println();
+        // }
+        // int sum=0;
+        // int count=0;
+        //  for(int i=0;i<arr.length;i++){
+        //     for(int j=0;j<arr[i].length;j++){
+        //         count++;
+//                 sum+=arr[i][j];
+//             }
+//         }
+//         System.out.println("sum is "+sum);
+//         System.out.println("count is "+count);
+//         System.out.println("average is "+(float)sum/(float)count);
+
+        
+//     }
+//}
