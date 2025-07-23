b8d83a8567f2ed66995511b4139833d07d93a0f1
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-23 13:31:47 +0530

    add

---
+++ Stack.java
@@ -0,0 +1,49 @@
+class StackDef{
+    int top=-1;
+    int n;
+    int arr[];
+    public StackDef(int n){
+        this.n=n;
+        arr=new int[n];
+    }
+    public void push(int data){
+        if(top!=n-1){
+            arr[++top]=data;
+        }
+        else{
+            System.out.println("Stack is full");
+        }
+    }
+    public void pop(){
+        if(top!=-1){
+           System.out.println("popped element "+ arr[top]);
+           top--;
+        
+        }
+        else{
+            System.out.println("Stack is Empty");
+        }
+
+    }
+    public void display(){
+    int temp=top;
+        while(temp!=-1){
+            System.out.print(arr[temp]+" ");
+            temp--;
+        }
+    }
+}
+
+
+public class Stack {
+    public static void main(String[] args) {
+        StackDef stk=new StackDef(5);
+        stk.push(5);
+        stk.push(4);
+        stk.push(3);
+        stk.push(2);
+        stk.pop();
+        stk.display();
+
+    }
+}
