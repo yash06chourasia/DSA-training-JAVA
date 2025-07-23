222155e1b2806e2b8c4547655d969db38912d194
yash06chourasia <yashchourasia06022004@gmail.com>
2025-07-23 12:27:46 +0530

    add

--- OOP_Java/Car.java
+++ OOP_Java/Car.java
@@ -8,11 +8,16 @@ public class Car{
 
     // Instance methods
     public void driving(){
+        if(fuelAvailable==0){
+            System.out.println("Car is out of fuel");
+            return;
+        }
         System.out.println("Car is driving");
         fuelAvailable--;
     }
-    public void addFuel(float fuel){
-        fuelAvailable+=fuel;
+    public void addFuel(float fuelAvailable){
+        this.fuelAvailable+=fuelAvailable;
+        // this resolves naming conflicts as well as used in constructor.It is also used to access instance variable of one class to another
     }
     public float getFuelAvialability(){
         return fuelAvailable;
--- OOP_Java/Driver.java
+++ OOP_Java/Driver.java
@@ -1,9 +1,21 @@
 public class Driver {
+    static int minAge=18;
+    // Static can be used outside scope without creating object
+    String name="Ram";
+    String dateOfLicense;
+    // For above two object creation is necessary to use
     public static void main(String[] args) {
         Car myCar=new Car();
-        myCar.fuelAvailable=15;
+        // myCar.fuelAvailable=15;
         myCar.driving();
         System.out.println(myCar.getFuelAvialability());
+        myCar.addFuel(6);
+        myCar.driving();
+        System.out.println(myCar.getFuelAvialability());
+        System.out.println(minAge);
+        Driver myDriver=new Driver();
+        // Object created to use non static member of a class
+        System.out.println(myDriver.name);
     }
 }
 
