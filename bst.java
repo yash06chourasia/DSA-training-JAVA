b897f9ded468a71b4e88a822034fae3b4d98c867
yash06chourasia <yashchourasia06022004@gmail.com>
2025-08-05 13:29:03 +0530

    add

---
+++ bst.java
@@ -0,0 +1,51 @@
+class Node{
+    Node left;
+    Node right;
+    int data;
+    Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+class Build{
+    static Node build(Node root,int val){
+        if(root==null){
+            return new Node(val);
+        }
+        if(root.data>val){
+            root.left=build(root.left,val);
+        }
+        else{
+            root.right=build(root.right,val);
+        }
+        return root;
+    }
+    static void search(Node root,int val){
+        if(root==null){
+            System.out.println("Empty tree");
+        }
+        if(root.data==val){
+            System.out.println("Element found");
+        }
+        if(val<root.data){
+            search(root.left,val);
+        }
+        if(val>root.data){
+            search(root.right,val);
+        }
+    }
+}
+
+public class bst {
+    public static void main(String[] args) {
+        int arr[]={5,1,24,6,4,6,46,78,9};
+        Build list=new Build();
+        Node root=null;
+        for(int val :arr){
+            root=list.build(root, val);
+        }
+        list.search(root, 4);
+    }
+    
+}
