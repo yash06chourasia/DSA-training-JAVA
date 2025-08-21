43c267339d91f102be8c287b3d5f816eb4def31c
yash06chourasia <yashchourasia06022004@gmail.com>
2025-08-21 14:29:34 +0530

    add

--- OOP_Java/inheritance.java
+++ OOP_Java/inheritance.java
@@ -11,6 +11,7 @@ class TwoWheeler extends Vehicle {
 public class inheritance {
     public static void main(String[] args) {
         Vehicle v = new Vehicle();
+        v.display();
         TwoWheeler two = new TwoWheeler();
         two.display(); 
     }
