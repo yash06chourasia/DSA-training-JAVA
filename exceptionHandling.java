bc9785cb48827c69e204a8c82e6723aa5274dd00
yash06chourasia <yashchourasia06022004@gmail.com>
2025-08-21 13:58:35 +0530

    add

--- exceptionHandling.java
+++ exceptionHandling.java
@@ -8,8 +8,8 @@ public class exceptionHandling {
         System.out.print("Enter Denominator : ");
         int d=inp.nextInt();
         try {
-            double div=(double)n/d;
-            System.out.printf("Division is %.3f",div);
+            int div=n/d;
+            System.out.printf("Integer Division is %d",div);
         } 
         catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero");
---
+++ testThrows.java
@@ -0,0 +1,19 @@
+public class testThrows {
+     static public void printName(String name)throws IllegalArgumentException{
+        if(name.contains("-")||name.contains(" ")){
+            throw new IllegalArgumentException();
+        }
+            System.out.println(name);
+        
+    }
+    public static void main(String[] args) {
+        try{
+            printName("Yash Chourasia");
+        }catch(IllegalArgumentException e){
+            System.out.println("Name cannot contain hyphens and space ");
+        }finally{
+            System.out.println("Im in finally block");
+        }
+        
+    }
+}
